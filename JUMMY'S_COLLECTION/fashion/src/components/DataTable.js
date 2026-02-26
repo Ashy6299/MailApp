0b2039aad6688ad9a38800c0aeeb73dd54a45deb
Adams <105866702+Ashy6299@users.noreply.github.com>
2026-02-26 15:08:30 +0100

    Delete JUMMY'S_COLLECTION directory

--- JUMMY'S_COLLECTION/fashion/src/components/DataTable.js
+++
@@ -1,9 +0,0 @@
-export const Tables = [
-  {
-    SN: 1,
-    Name: "Adams",
-    Surname: "Ashraff",
-    DOB: "June",
-    Email: "adamsashraf80@gmail.com",
-  },
-];
--- JUMMY'S_COLLECTION/fashion/src/components/Datas.js
+++
@@ -1,9 +0,0 @@
-export const datas = [
-  {
-    id: 1,
-    Name: "Adams",
-    Surname: "Ashraff",
-    DOB: "June",
-    Email: "adamsashraf80@gmail.com",
-  },
-];
